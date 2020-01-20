0475da601fdc913b2ba954ff8ed9afa796f5113c
Kray-G <nagai.kazuya@gmail.com>
2020-01-20 12:31:59 +0900

    moved extlibs.

--- make.cmd
+++ make.cmd
@@ -18,15 +18,16 @@ del lexer.obj
 move kx.tab.c src/parser.c
 move kx.tab.h include/parser.tab.h
 
-set OBJS=getopt.obj string.obj parser.obj lexer.obj main.obj allocator.obj kstr.obj bign.obj bigz.obj loadlib.obj global.obj
+set OBJS=getopt.obj string.obj parser.obj lexer.obj main.obj allocator.obj allocutil.obj kstr.obj bign.obj bigz.obj loadlib.obj global.obj
 set OBJS=%OBJS% ast_object.obj ast_display.obj ast_analyzer.obj ast_gencode.obj
 set OBJS=%OBJS% ir_fix.obj ir_dump.obj ir_exec.obj ir_util.obj
 REM del %OBJS%
 call :COMPILE %OBJS%
 cl %CFLAGS% /Fekinx.exe %OBJS%
-timeit cl -LD -nologo %CFLAGS% src/kxsystem.c bign.obj bigz.obj
-timeit cl -LD -nologo %CFLAGS% src/kxstring.c bign.obj bigz.obj
-timeit cl -LD -nologo %CFLAGS% src/kxarray.c  bign.obj bigz.obj
+timeit cl /LD /nologo %CFLAGS% src/extlib/kxsystem.c bign.obj bigz.obj
+timeit cl /LD /nologo %CFLAGS% src/extlib/kxstring.c bign.obj bigz.obj
+timeit cl /LD /nologo %CFLAGS% src/extlib/kxarray.c  bign.obj bigz.obj
+timeit cl /LD /nologo %CFLAGS% src/extlib/kxregex.c  bign.obj bigz.obj kstr.obj allocutil.obj
 goto END
 
 :TEST_CODE
@@ -80,6 +81,7 @@ if not exist bign.obj       timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/
 if not exist bigz.obj       timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/bigz.c
 if not exist ir_dump.obj    timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/ir_dump.c
 if not exist allocator.obj  timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/allocator.c
+if not exist allocutil.obj  timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/allocutil.c
 if not exist global.obj     timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/global.c
 if not exist loadlib.obj    timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/loadlib.c
 if not exist ir_util.obj    timeit cl /c /nologo /DKX_EXEC_NODEBUG %CFLAGS% src/ir_util.c
@@ -89,7 +91,7 @@ exit /b 0
 :TEST_EXEC
 call :MKDOTS %1
 set /P _DUMMY=Starting [%1] %DOTS% < NUL
-cl /nologo /DKX_EXEC_NODEBUG %CFLAGS% /Fetest.exe src/exec/test/%1.c ir_exec.obj ir_util.obj ir_dump.obj allocator.obj kstr.obj bign.obj bigz.obj string.obj global.obj loadlib.obj
+cl /nologo /DKX_EXEC_NODEBUG %CFLAGS% /Fetest.exe src/exec/test/%1.c ir_exec.obj ir_util.obj ir_dump.obj allocator.obj allocutil.obj kstr.obj bign.obj bigz.obj string.obj global.obj loadlib.obj
 test.exe
 if ERRORLEVEL 1 goto FAILED
 echo Successful
