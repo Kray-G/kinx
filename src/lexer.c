be53e41914e8a62c0e565f040c7bff01132acada
Kray-G <nagai.kazuya@gmail.com>
2020-01-23 14:09:55 +0900

    supported arguments for the program.

--- src/lexer.c
+++ src/lexer.c
@@ -463,7 +463,7 @@ HEAD_OF_YYLEX:
             return MODEQ;
         }
         return '%';
-    case '_':
+    case '_': case '$':
     case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm':
     case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
     case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M':
