4b277f35a1edfd3612a1129bc5e82eb320e25e36
Kray-G <nagai.kazuya@gmail.com>
2020-01-23 16:34:51 +0900

    updated for linux.

--- src/global.c
+++ src/global.c
@@ -9,4 +9,9 @@ kvec_t(kx_lexinfo_t) kx_lex_stack;
 kx_object_t *kx_obj_mgr = NULL;
 kx_object_t *kx_ast_root = NULL;
 
+#if defined(_WIN32) || defined(_WIN64)
+#else
+void *jumptable[] = { KX_LABELS };
+#endif
+
 KX_DECL_MEM_ALLOCATORS();
