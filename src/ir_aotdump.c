39496c2b3510a802c3a4ebb35b1950ca007f59be
Kray-G <nagai.kazuya@gmail.com>
2020-01-30 19:32:33 +0900

    updated disasm.

--- src/ir_aotdump.c
+++ src/ir_aotdump.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <jit.h>
 #include "disasm/disas.h"
 
-void native_dump(unsigned char *bytes, int size, int base)
+void native_dump(unsigned char *bytes, int size)
 {
+    #if defined(SLJIT_CONFIG_X86_64)
     struct disassembler *ds = ds_init(X86_ARCH, MODE_64B);
+    #elif defined(SLJIT_CONFIG_ARM_64)
+    struct disassembler *ds = ds_init(ARM_ARCH, MODE_64B);
+    #elif defined(SLJIT_CONFIG_MIPS_64)
+    struct disassembler *ds = ds_init(MIPS_ARCH, MODE_64B);
+    #endif
 
-    ds_decode(ds, bytes, size, (uint64_t)bytes);
+    #if defined(SLJIT_CONFIG_X86_64) || defined(SLJIT_CONFIG_ARM_64) || defined(SLJIT_CONFIG_MIPS_64)
+    ds_decode(ds, bytes, size, 0x0);
     struct dis *dis = NULL;
 
 	int biter = 0;
 	DS_FOREACH(ds, dis) {
-		printf("%8llx:   ", dis->address - base);
+		printf("%8llx:   ", dis->address);
 		for (int m = 0, t = dis->used_bytes; m < 10; m++, t = m < dis->used_bytes) {
             if (t) {
                 printf("%02x ", bytes[biter++]);
@@ -22,4 +30,7 @@ void native_dump(unsigned char *bytes, int size, int base)
 	}
 
     ds_destroy(ds);
+    #else
+    return;
+    #endif
 }
