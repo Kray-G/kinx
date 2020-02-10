e99cd70c0da453907418a7f143041dca1340892e
Kray-G <nagai.kazuya@gmail.com>
2020-02-10 11:00:33 +0900

    updated a native function.

--- src/ir_natutil.c
+++ src/ir_natutil.c
@@ -9,6 +9,12 @@ int64_t kxn_print_val(sljit_sw *val)
     return 0;
 }
 
+int64_t kxn_print_index(sljit_sw *val)
+{
+    printf("index = %"PRId64", %p\n", val[0], (void*)val[0]);
+    return 0;
+}
+
 int64_t call_native(kx_context_t *ctx, kx_frm_t *frmv, int count, kx_fnc_t *nfnc)
 {
     kx_native_funcp_t func = nfnc->native.func;
@@ -54,15 +60,20 @@ int64_t call_native(kx_context_t *ctx, kx_frm_t *frmv, int count, kx_fnc_t *nfnc
 
     kv_shrink(ctx->stack, count);
     push_i(ctx->stack, v);
-    if (info[4] != 0) {
-        return info[5] ? info[5] : KX_NAT_UNKNOWN_ERROR;
+    if (info[KXN_EXC_FLAG] != 0) {
+        return info[KXN_EXC_CODE] ? info[KXN_EXC_CODE] : KX_NAT_UNKNOWN_ERROR;
     }
     return 0;
 }
 
-static int64_t native_get_var_int_of(kx_context_t *ctx, kx_frm_t *frm, int index)
+static sljit_sw native_get_var_int_of(sljit_sw *info, kx_context_t *ctx, kx_frm_t *frm, int index)
 {
-    return (kv_A(frm->v, index)).value.iv;
+    if ((kv_A(frm->v, index)).type == KX_INT_T) {
+        return (sljit_sw)(kv_A(frm->v, index)).value.iv;
+    }
+    info[KXN_EXC_FLAG] = 1;
+    info[KXN_EXC_CODE] = KX_NAT_TYPE_MISMATCH;
+    return 0;
 }
 
 sljit_sw native_get_var_int(sljit_sw *args)
@@ -72,19 +83,48 @@ sljit_sw native_get_var_int(sljit_sw *args)
     int64_t index = (int64_t)args[2];
     kx_context_t *ctx = (kx_context_t *)info[0];
     if (lex == 0) {
-        return (sljit_sw)native_get_var_int_of(ctx, (kx_frm_t *)info[1], index);
+        return native_get_var_int_of(info, ctx, (kx_frm_t *)info[1], index);
     } else if (lex == 1) {
-        return (sljit_sw)native_get_var_int_of(ctx, (kx_frm_t *)info[2], index);
+        return native_get_var_int_of(info, ctx, (kx_frm_t *)info[2], index);
     }
     kx_frm_t *lexv = (kx_frm_t *)info[2];
     while (lexv && --lex) {
         lexv = lexv->lex;
     }
-    return (sljit_sw)native_get_var_int_of(ctx, lexv, index);
+    return native_get_var_int_of(info, ctx, lexv, index);
 }
 
-static int64_t *native_get_var_int_addr_of(kx_context_t *ctx, kx_frm_t *frm, int index)
+static sljit_sw native_get_var_nfunc_of(sljit_sw *info, kx_context_t *ctx, kx_frm_t *frm, int index)
 {
+    if ((kv_A(frm->v, index)).type == KX_NFNC_T) {
+        return (sljit_sw)(kv_A(frm->v, index)).value.fn->native.func;
+    }
+    info[KXN_EXC_FLAG] = 1;
+    info[KXN_EXC_CODE] = KX_NAT_TYPE_MISMATCH;
+    return 0;
+}
+
+sljit_sw native_get_var_nfunc(sljit_sw *args)
+{
+    sljit_sw *info = (sljit_sw *)args[0];
+    int64_t lex = (int64_t)args[1];
+    int64_t index = (int64_t)args[2];
+    kx_context_t *ctx = (kx_context_t *)info[0];
+    if (lex == 0) {
+        return native_get_var_nfunc_of(info, ctx, (kx_frm_t *)info[1], index);
+    } else if (lex == 1) {
+        return native_get_var_nfunc_of(info, ctx, (kx_frm_t *)info[2], index);
+    }
+    kx_frm_t *lexv = (kx_frm_t *)info[2];
+    while (lexv && --lex) {
+        lexv = lexv->lex;
+    }
+    return native_get_var_nfunc_of(info, ctx, lexv, index);
+}
+
+static int64_t *native_get_var_int_addr_of(sljit_sw *info, kx_context_t *ctx, kx_frm_t *frm, int index)
+{
+    (kv_A(frm->v, index)).type = KX_INT_T;  /* make it integer, because it is l-value */
     return &((kv_A(frm->v, index)).value.iv);
 }
 
@@ -95,13 +135,13 @@ sljit_sw native_get_var_int_addr(sljit_sw *args)
     int64_t index = (int64_t)args[2];
     kx_context_t *ctx = (kx_context_t *)info[0];
     if (lex == 0) {
-        return (sljit_sw)native_get_var_int_addr_of(ctx, (kx_frm_t *)info[1], index);
+        return (sljit_sw)native_get_var_int_addr_of(info, ctx, (kx_frm_t *)info[1], index);
     } else if (lex == 1) {
-        return (sljit_sw)native_get_var_int_addr_of(ctx, (kx_frm_t *)info[2], index);
+        return (sljit_sw)native_get_var_int_addr_of(info, ctx, (kx_frm_t *)info[2], index);
     }
     kx_frm_t *lexv = (kx_frm_t *)info[2];
     while (lexv && --lex) {
         lexv = lexv->lex;
     }
-    return (sljit_sw)native_get_var_int_addr_of(ctx, lexv, index);
+    return (sljit_sw)native_get_var_int_addr_of(info, ctx, lexv, index);
 }
