129fa25b2861200cb5b6ab51fa5960ffb654a7e2
Kray-G <nagai.kazuya@gmail.com>
2020-02-10 19:12:10 +0900

    added File object.

--- src/loadlib.c
+++ src/loadlib.c
@@ -1,5 +1,6 @@
 #include <dbg.h>
 #include <stdio.h>
+#include <fileutil.h>
 
 extern const char *alloc_string(const char *str);
 
@@ -9,87 +10,6 @@ extern const char *alloc_string(const char *str);
 
 #if !defined(KCC_WINDOWS)
 #include <dlfcn.h>
-#endif
-
-/*
- *  Path utilities.
- */
-
-#if defined(KCC_WINDOWS)
-#include <windows.h>
-#ifndef MAX_PATH
-#define MAX_PATH 260
-#endif
-#ifndef PATH_MAX
-#define PATH_MAX MAX_PATH
-#endif
-#define SUFFIX      ".exe"
-#define PUTENV      _putenv
-#define SNPRINTF    _snprintf
-#define DELIM       ";"
-#define PATH_DELIM  "\\"
-int file_exists(const char *p)
-{
-    unsigned long attr = GetFileAttributes(p);
-    if (attr == 0xFFFFFFFF) {
-        return 0;
-    }
-    return 1;
-}
-
-char* get_exe_path(void)
-{
-    static char s_result[2048] = {0};
-
-    if (!s_result[0]) {
-        char* p;
-        int  len;
-        char exe_full_path[PATH_MAX];
-
-        len = GetModuleFileNameA(NULL, exe_full_path, PATH_MAX);
-        if (len > 0) {
-            strncpy(s_result, exe_full_path, 2040);
-            p = strrchr(s_result, '\\');
-            if (p) *p = 0;
-        }
-    }
-
-    return s_result;
-}
-#else
-#include <linux/limits.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <limits.h>
-#include <string.h>
-#include <unistd.h>
-#define SUFFIX
-#define PUTENV      putenv
-#define SNPRINTF    snprintf
-#define DELIM       ":"
-#define PATH_DELIM  "/"
-int file_exists(const char *p)
-{
-    struct stat st;
-    return stat(p, &st) == 0 ? 1 : 0;
-}
-
-char* get_exe_path(void)
-{
-    static char s_result[2048] = {0};
-
-    if (!s_result[0]) {
-        char* p;
-        char exe_full_path[PATH_MAX];
-        readlink("/proc/self/exe", exe_full_path, PATH_MAX);
-        strncpy(s_result, exe_full_path, 2040);
-        p = strrchr(s_result, '/');
-        if (p) *p = 0;
-    }
-
-    return s_result;
-}
 #endif
 
 #if defined(KCC_WINDOWS)
