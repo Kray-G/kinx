eeb712d260d458d7a22fb2bc8e117a4252a4b28f
Kray-G <nagai.kazuya@gmail.com>
2020-02-21 21:25:29 +0900

    updated for Linux.

--- src/extlib/kc-json/kc-json.c
+++ src/extlib/kc-json/kc-json.c
@@ -1,6 +1,6 @@
 #include "kc-json.h"
 
-static inline string_t string_init_alloc(const char *cstr)
+string_t string_init_alloc(const char *cstr)
 {
     unsigned int len = strlen(cstr);
     unsigned int cap = KCC_CAPACITY(len);
@@ -14,7 +14,7 @@ static inline string_t string_init_alloc(const char *cstr)
     };
 }
 
-static inline string_t string_substr(const string_t str, int start, int len)
+string_t string_substr(const string_t str, int start, int len)
 {
     unsigned int end = len < 0 ? str.len : (start + len);
     if (0 < len && str.len < end) {
