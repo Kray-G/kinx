f1f1a80655d3ffdf296399e8639bb4932c93ca6c
Kray-G <nagai.kazuya@gmail.com>
2020-03-09 13:18:23 +0900

    updated system classes/functions.

--- src/ast_native.c
+++ src/ast_native.c
@@ -1127,6 +1127,10 @@ static void nativejit_ast(kx_native_context_t *nctx, kx_object_t *node, int lval
     case KXST_MIXIN:
         kx_yyerror_line("Not supported operation in native function", node->file, node->line);
         break;
+    case KXST_SYSCLASS: {
+        kx_yyerror_line("Not supported operation in native function", node->file, node->line);
+        break;
+    }
     case KXST_CLASS: {    /* s: name, lhs: arglist, rhs: block: ex: expr (inherit) */
         kx_yyerror_line("Not supported operation in native function", node->file, node->line);
         break;
