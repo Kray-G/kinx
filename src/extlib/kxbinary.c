24d0d7dacc5e5cd818c25d421e64b17cf04cebb1
Kray-G <nagai.kazuya@gmail.com>
2020-04-14 18:15:02 +0900

    supported some binary methods.

--- src/extlib/kxbinary.c
+++ src/extlib/kxbinary.c
@@ -3,6 +3,14 @@
 
 KX_DECL_MEM_ALLOCATORS();
 
+static int throw_invalid_object(int args, kx_context_t *ctx)
+{
+    KX_ADJST_STACK();
+    push_s(ctx->stack, "SystemException");
+    push_s(ctx->stack, "Invalid object, it must be a binary");
+    return KX_THROW_EXCEPTION;
+}
+
 int Binary_length(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
 {
     kx_bin_t *bin = get_arg_bin(1, args, ctx);
@@ -12,11 +20,203 @@ int Binary_length(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
         return 0;
     }
 
-    KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object, it must be a binary");
+    return throw_invalid_object(args, ctx);
+}
+
+int Binary_push(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        kx_val_t *val = &kv_last_by(ctx->stack, 2);
+        switch (val->type) {
+        case KX_UND_T:
+            *kv_pushp(uint8_t, bin->bin) = 0;
+            break;
+        case KX_INT_T:
+            *kv_pushp(uint8_t, bin->bin) = (val->value.iv & 0xFF);
+            break;
+        case KX_BIG_T:
+            KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object, cannot push to a binary");
+            break;
+        case KX_DBL_T:
+            *kv_pushp(uint8_t, bin->bin) = ((int)(val->value.dv) & 0xFF);
+            break;
+        case KX_CSTR_T:
+            *kv_pushp(uint8_t, bin->bin) = (val->value.pv[0]);
+            break;
+        case KX_STR_T:
+            *kv_pushp(uint8_t, bin->bin) = (ks_string(val->value.sv)[0]);
+            break;
+        case KX_BIN_T:
+            kv_append(uint8_t, bin->bin, val->value.bn->bin);
+            break;
+        case KX_OBJ_T:
+        case KX_FNC_T:
+        case KX_BFNC_T:
+            KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object, cannot push to a binary");
+            break;
+        }
+
+        KX_ADJST_STACK();
+        push_bin(ctx->stack, bin);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
+}
+
+int Binary_pop(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        int sz = kv_size(bin->bin);
+        KX_ADJST_STACK();
+        if (sz == 0) {
+            push_undef(ctx->stack);
+            return 0;
+        }
+        uint8_t val = kv_pop(bin->bin);
+        push_i(ctx->stack, val);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
+}
+
+int Binary_unshift(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        kvec_t(uint8_t) v;
+        kv_init(v);
+        kx_val_t *val = &kv_last_by(ctx->stack, 2);
+        switch (val->type) {
+        case KX_UND_T:
+            *kv_pushp(uint8_t, v) = 0;
+            kv_prepend(uint8_t, bin->bin, v);
+            break;
+        case KX_INT_T:
+            *kv_pushp(uint8_t, v) = (val->value.iv & 0xFF);
+            kv_prepend(uint8_t, bin->bin, v);
+            break;
+        case KX_BIG_T:
+            KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object, cannot push to a binary");
+            break;
+        case KX_DBL_T:
+            *kv_pushp(uint8_t, v) = ((int)(val->value.dv) & 0xFF);
+            kv_prepend(uint8_t, bin->bin, v);
+            break;
+        case KX_CSTR_T:
+            *kv_pushp(uint8_t, v) = (val->value.pv[0]);
+            kv_prepend(uint8_t, bin->bin, v);
+            break;
+        case KX_STR_T:
+            *kv_pushp(uint8_t, v) = (ks_string(val->value.sv)[0]);
+            kv_prepend(uint8_t, bin->bin, v);
+            break;
+        case KX_BIN_T:
+            kv_prepend(uint8_t, bin->bin, val->value.bn->bin);
+            break;
+        case KX_OBJ_T:
+        case KX_FNC_T:
+        case KX_BFNC_T:
+            KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object, cannot push to a binary");
+            break;
+        }
+
+        KX_ADJST_STACK();
+        push_bin(ctx->stack, bin);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
+}
+
+int Binary_shift(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        int sz = kv_size(bin->bin);
+        KX_ADJST_STACK();
+        if (sz == 0) {
+            push_undef(ctx->stack);
+            return 0;
+        }
+        uint8_t val = kv_A(bin->bin, 0);
+        kv_shift(uint8_t, bin->bin);
+        push_i(ctx->stack, val);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
+}
+
+static kstr_t * Binary_join_impl(kx_bin_t *bin, const char *delm, kx_context_t *ctx)
+{
+    int sz = kv_size(bin->bin);
+    kstr_t *str = allocate_str(ctx);
+    ks_append(str, "<");
+    for (int i = 0; i < sz; ++i) {
+        if (delm && i != 0) {
+            ks_append(str, delm);
+        }
+        ks_appendf(str, "0x%02x", kv_A(bin->bin, i));
+    }
+    ks_append(str, ">");
+    return str;
+}
+
+int Binary_join(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        const char *delm = get_arg_str(2, args, ctx);
+        kstr_t *str = Binary_join_impl(bin, delm, ctx);
+        KX_ADJST_STACK();
+        push_sv(ctx->stack, str);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
+}
+
+int Binary_toString(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        const char *delm = get_arg_str(2, args, ctx);
+        kstr_t *str = Binary_join_impl(bin, ", ", ctx);
+        KX_ADJST_STACK();
+        push_sv(ctx->stack, str);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
+}
+
+int Binary_clone(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
+{
+    kx_bin_t *bin = get_arg_bin(1, args, ctx);
+    if (bin) {
+        kx_bin_t *dst = allocate_bin(ctx);
+        kv_copy(uint8_t, dst->bin, bin->bin);
+        KX_ADJST_STACK();
+        push_bin(ctx->stack, bin);
+        return 0;
+    }
+
+    return throw_invalid_object(args, ctx);
 }
 
 static kx_bltin_def_t kx_bltin_info[] = {
     { "length", Binary_length },
+    { "push", Binary_push },
+    { "pop", Binary_pop },
+    { "unshift", Binary_unshift },
+    { "shift", Binary_shift },
+    { "join", Binary_join },
+    { "toString", Binary_toString },
+    { "clone", Binary_clone },
 };
 
 KX_DLL_DECL_FNCTIONS(kx_bltin_info, NULL, NULL);
