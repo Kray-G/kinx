343524d48f6c27eef98a0f9573ef3e5af1a088ca
Kray-G <nagai.kazuya@gmail.com>
2020-05-29 17:15:27 +0900

    a little modification of class definition.

--- src/ast_object.c
+++ src/ast_object.c
@@ -424,11 +424,8 @@ kx_object_t *kx_gen_func_object(int type, int optional, const char *name, kx_obj
             rhs,
             kx_gen_stmt_object(KXST_EXPR,
                 kx_gen_bassign_object(KXOP_ASSIGN,
-                    kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object(name, KX_UNKNOWN_T), kx_gen_str_object("_className")),
-                    kx_gen_bassign_object(KXOP_ASSIGN,
-                        kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object("this", KX_UNKNOWN_T), kx_gen_str_object("_className")),
-                        kx_gen_str_object(name)
-                    )
+                    kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object("this", KX_UNKNOWN_T), kx_gen_str_object("_className")),
+                    kx_gen_str_object(name)
                 ),
             NULL, NULL)
         );
@@ -436,11 +433,8 @@ kx_object_t *kx_gen_func_object(int type, int optional, const char *name, kx_obj
             rhs,
             kx_gen_stmt_object(KXST_EXPR,
                 kx_gen_bassign_object(KXOP_ASSIGN,
-                    kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object(name, KX_UNKNOWN_T), kx_gen_str_object("_classid")),
-                    kx_gen_bassign_object(KXOP_ASSIGN,
-                        kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object("this", KX_UNKNOWN_T), kx_gen_str_object("_classid")),
-                        kx_gen_bassign_object(KXOP_ASSIGN, kx_gen_var_object("_classid", KX_UNKNOWN_T), kx_gen_int_object(classid))
-                    )
+                    kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object("this", KX_UNKNOWN_T), kx_gen_str_object("_classid")),
+                    kx_gen_bassign_object(KXOP_ASSIGN, kx_gen_var_object("_classid", KX_UNKNOWN_T), kx_gen_int_object(classid))
                 ),
             NULL, NULL)
         );
@@ -509,6 +503,20 @@ kx_object_t *kx_gen_func_object(int type, int optional, const char *name, kx_obj
         } else if (optional == KXFT_CLASS) {
             assign = kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object(name, KX_OBJ_T), kx_gen_str_object("create"));
             assign = kx_gen_bassign_object(KXOP_DECL, assign, obj);
+            assign = kx_gen_bexpr_object(KXST_STMTLIST,
+                assign,
+                kx_gen_bassign_object(KXOP_ASSIGN,
+                    kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object(name, KX_UNKNOWN_T), kx_gen_str_object("_className")),
+                    kx_gen_str_object(name)
+                )
+            );
+            assign = kx_gen_bexpr_object(KXST_STMTLIST,
+                assign,
+                kx_gen_bassign_object(KXOP_ASSIGN,
+                    kx_gen_bexpr_object(KXOP_IDX, kx_gen_var_object(name, KX_UNKNOWN_T), kx_gen_str_object("_classid")),
+                    kx_gen_bassign_object(KXOP_ASSIGN, kx_gen_var_object("_classid", KX_UNKNOWN_T), kx_gen_int_object(classid))
+                )
+            );
         } else {
             kx_object_t *v = kx_gen_var_object(name, KX_UNKNOWN_T);
             v->optional = KXDC_CONST;
