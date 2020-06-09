d485ec9b91c53b7478f974315fb67b600276905b
Kray-G <nagai.kazuya@gmail.com>
2020-06-09 21:22:10 +0900

    updated as a library.

--- examples/c/runkinx.c
+++ examples/c/runkinx.c
@@ -2,19 +2,29 @@
 
 int build_and_run_script(kinx_compiler *kc)
 {
-    const char *filename = "sample.kx";
-    if (!kinx_load_file(kc, filename)) {
+    kinx_add_argument(kc, "34");
+
+    const char *filename = "examples/fib_n.kx";
+    if (!kinx_loadfile(kc, filename)) {
         printf("Failed to load the file: %s\n", filename);
         return 1;
     }
+    kinx_add_code(kc, "return r;");
+
     return kinx_run(kc);
 }
 
 int main(int ac, char **av)
 {
-    kinx_compiler *kc = kinx_initialize();
+    kinx_compiler *kc = kinx_create_compiler();
+    if (!kc) {
+        return 1;
+    }
+
     int r = build_and_run_script(kc);
     printf("r = %d\n", r);
+    printf("compile time = %8.3f\n", kc->timer.compile);
+    printf("running time = %8.3f\n", kc->timer.runtime);
     kinx_finalize(kc);
     return r;
 }
