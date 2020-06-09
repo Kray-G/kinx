35d799d21605cc38722dd4d39b07ea43fa062fef
Kray-G <nagai.kazuya@gmail.com>
2020-06-09 13:41:39 +0900

    updated on Linux.

--- include/jit.h
+++ include/jit.h
@@ -8,17 +8,17 @@
 #error Architecture shoule be 64bit.
 #endif
 
-static inline struct sljit_compiler *new_compiler(void);
-static inline void *finalize_code(struct sljit_compiler *C);
-static inline int get_code_code(struct sljit_compiler *C);
-static inline void free_code(void *code);
-static inline void jit_run(struct sljit_compiler *C);
+extern struct sljit_compiler *new_compiler(void);
+extern void *finalize_code(struct sljit_compiler *C);
+extern int get_code_code(struct sljit_compiler *C);
+extern void free_code(void *code);
+extern void jit_run(struct sljit_compiler *C);
 
 #if defined(_WIN32) || defined(_WIN64) || defined(__x86_64__)
 #include "../src/disasm-x64/udis86.h"
 #define USE_DISASM_X64 (1)
-static inline int disasm_hex(struct ud* u) ;
-static inline void disasm_code(void *code, int size);
+extern int disasm_hex(struct ud* u);
+extern void disasm_code(void *code, int size);
 #else
 #include "disasm/disas.h"
 #define USE_DISASM_X64 (0)
