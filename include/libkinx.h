45d594bae4cdb8638bc88457bebb49c68a074393
Kray-G <nagai.kazuya@gmail.com>
2020-06-10 10:59:33 +0900

    updated C interface.

--- include/libkinx.h
+++ include/libkinx.h
@@ -1,8 +1,11 @@
 #ifndef KX_LIBKINX_H
 #define KX_LIBKINX_H
-// #undef KX_LIB_DLL
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <inttypes.h>
 #include <fileutil.h>
 
 #ifndef DllExport
@@ -35,6 +38,10 @@ typedef int (*kinx_run_t)(struct kinx_compiler_ *kc);
 typedef int (*kinx_add_argument_t)(struct kinx_compiler_ *kc, const char *arg);
 typedef void (*kinx_free_compiler_t)(struct kinx_compiler_ *kc);
 
+typedef int64_t (*kinx_get_argument_as_int_t)(struct kinx_compiler_ *kc, int i);
+typedef double (*kinx_get_argument_as_dbl_t)(struct kinx_compiler_ *kc, int i);
+typedef const char *(*kinx_get_argument_as_str_t)(struct kinx_compiler_ *kc, int i);
+typedef int (*kinx_get_argument_type_t)(struct kinx_compiler_ *kc, int i);
 
 typedef struct kinx_timer_ {
     systemtimer_t v;
@@ -43,18 +50,22 @@ typedef struct kinx_timer_ {
 } kinx_timer_t;
 
 typedef struct kinx_compiler_ {
-    void                 *ctx;
-    void                 *h;
-    void                 *code; /* typeof kstr_t* */
-    int                  is_main_context;
-    int                  ac;
-    char                 *av[KX_LIB_MAX_ARGS];
-    kinx_timer_t         timer;
-    kinx_loadfile_t      loadfile;
-    kinx_add_code_t      add_code;
-    kinx_run_t           run;
-    kinx_add_argument_t  add_argument;
-    kinx_free_compiler_t finalize;
+    void *ctx;
+    void *h;
+    void *code; /* typeof kstr_t* */
+    int  is_main_context;
+    int  ac;
+    char *av[KX_LIB_MAX_ARGS];
+    kinx_timer_t               timer;
+    kinx_loadfile_t            loadfile;
+    kinx_add_code_t            add_code;
+    kinx_run_t                 run;
+    kinx_add_argument_t        add_argument;
+    kinx_free_compiler_t       finalize;
+    kinx_get_argument_type_t   get_argument_type;
+    kinx_get_argument_as_int_t get_argument_as_int;
+    kinx_get_argument_as_dbl_t get_argument_as_dbl;
+    kinx_get_argument_as_str_t get_argument_as_str;
 } kinx_compiler;
 
 typedef int (*kinx_do_main_t)(int ac, char **av);
@@ -133,6 +144,32 @@ static inline void unload_kinx_dll(void *h)
 #endif
 
 #ifndef KX_LIB_DLL
+/*
+    Copy from ir.h. Don't forget copy this again if it has changed.
+*/
+enum irexec {
+    KX_UNKNOWN_T = -1,
+    KX_UND_T = 0,   /* undefined(null) must be 0 because it becomes undefined after clearing with 0. */
+    KX_INT_T,
+    KX_BIG_T,       /* Big Integer */
+    KX_DBL_T,
+    KX_CSTR_T,
+    KX_STR_T,
+    KX_BIN_T,
+    KX_LVAL_T,
+    KX_OBJ_T,       /* ARRAY is also object */
+    KX_FNC_T,
+    KX_FRM_T,
+    KX_BFNC_T,
+    KX_NFNC_T,
+    KX_ADDR_T,
+    KX_ANY_T,
+    KX_SPR_T,       /* spread-specified varable */
+    KX_COR_T,       /* coroutine start */
+    KX_ARY_T,       /* used only with typeof */
+    KX_DEF_T,       /* used only with typeof */
+};
+
 static inline int kinx_call_main(int ac, char **av)
 {
     void *h = load_kinx_dll();
@@ -199,6 +236,29 @@ static inline int kinx_add_code(kinx_compiler *kc, const char *code)
     return kc->add_code(kc, code);
 }
 
+static inline int kinx_add_codef(kinx_compiler *kc, const char *fmt, ...)
+{
+    if (!kc) {
+        return 0;
+    }
+
+    char buf[2048] = {0};
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, 2040, fmt, ap);
+    va_end(ap);
+ 
+    return kc->add_code(kc, buf);
+}
+
+static inline int kinx_add_callback(kinx_compiler *kc, const char *name, void *func)
+{
+    if (!kc) {
+        return 0;
+    }
+    return kinx_add_codef(kc, "C.setFunction(\"%s\", 0x%"PRIx64", 0x%"PRIx64");", name, kc, func);
+}
+
 static inline int kinx_run(kinx_compiler *kc)
 {
     if (!kc) {
@@ -213,6 +273,38 @@ static inline int kinx_add_argument(kinx_compiler *kc, const char *arg)
         return 0;
     }
     return kc->add_argument(kc, arg);
+}
+
+static inline int kinx_get_argument_type(kinx_compiler *kc, int i)
+{
+    if (!kc) {
+        return 0;
+    }
+    return kc->get_argument_type(kc, i);
+}
+
+static inline int kinx_get_argument_as_int(kinx_compiler *kc, int i)
+{
+    if (!kc) {
+        return 0;
+    }
+    return kc->get_argument_as_int(kc, i);
+}
+
+static inline double kinx_get_argument_as_dbl(kinx_compiler *kc, int i)
+{
+    if (!kc) {
+        return 0;
+    }
+    return kc->get_argument_as_dbl(kc, i);
+}
+
+static inline const char *kinx_get_argument_as_str(kinx_compiler *kc, int i)
+{
+    if (!kc) {
+        return 0;
+    }
+    return kc->get_argument_as_str(kc, i);
 }
 #endif
 
