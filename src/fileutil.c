757c94fe71abb2cd5ecdf20be0463b9859414fd6
Kray-G <nagai.kazuya@gmail.com>
2020-09-14 13:12:21 +0900

    improved a using directive.

--- src/fileutil.c
+++ src/fileutil.c
@@ -7,6 +7,13 @@
  */
 
 #if defined(KCC_WINDOWS)
+typedef struct kx_dir_impl_ {
+    void            *find_handle;
+    WIN32_FIND_DATAW find_data;
+    struct dirent    entry;
+    uint8_t          end;
+} kx_dir_impl_t;
+
 int file_exists(const char *p)
 {
     unsigned long attr = GetFileAttributes(p);
@@ -35,6 +42,104 @@ char* get_exe_path(void)
 
     return s_result;
 }
+
+wchar_t *kx_mb2unicode(const char *string, int32_t encoding)
+{
+    wchar_t *string_wide = NULL;
+    uint32_t string_wide_size = 0;
+
+    string_wide_size = MultiByteToWideChar(encoding, 0, string, -1, NULL, 0);
+    if (string_wide_size == 0) {
+        return NULL;
+    }
+    string_wide = (wchar_t *)kx_calloc((string_wide_size + 1), sizeof(wchar_t));
+    if (string_wide == NULL) {
+        return NULL;
+    }
+    memset(string_wide, 0, sizeof(wchar_t) * (string_wide_size + 1));
+    MultiByteToWideChar(encoding, 0, string, -1, string_wide, string_wide_size);
+
+    return string_wide;
+}
+
+void kx_free_unicode(wchar_t **string)
+{
+    if (string != NULL) {
+        kx_free(*string);
+        *string = NULL;
+    }
+}
+
+kx_dir_t *kx_open_dir(const char *path)
+{
+    WIN32_FIND_DATAW find_data;
+    kx_dir_impl_t *dir = NULL;
+    wchar_t *path_wide = NULL;
+    char fixed_path[320];
+    void *handle = NULL;
+
+
+    if (path == NULL) {
+        return NULL;
+    }
+
+    strncpy(fixed_path, path, sizeof(fixed_path) - 1);
+    fixed_path[sizeof(fixed_path) - 1] = 0;
+    strcat(fixed_path, "\\*");
+    path_wide = kx_mb2unicode(fixed_path, CP_UTF8);
+    if (path_wide == NULL) {
+        return NULL;
+    }
+    handle = FindFirstFileW(path_wide, &find_data);
+    kx_free_unicode(&path_wide);
+    if (handle == INVALID_HANDLE_VALUE) {
+        return NULL;
+    }
+    dir = (kx_dir_impl_t *)kx_calloc(1, sizeof(kx_dir_impl_t));
+    if (dir == NULL) {
+        return NULL;
+    }
+    dir->find_handle = handle;
+    dir->end = 0;
+    memcpy(&dir->find_data, &find_data, sizeof(dir->find_data));
+
+    return (kx_dir_t *)dir;
+}
+
+kx_dirent_t* kx_read_dir(kx_dir_t *dirimpl)
+{
+    if (dirimpl == NULL) {
+        return NULL;
+    }
+    kx_dir_impl_t *dir = (kx_dir_impl_t *)dirimpl;
+    if (dir->end) {
+        return NULL;
+    }
+
+    WideCharToMultiByte(CP_UTF8, 0, dir->find_data.cFileName, -1, dir->entry.d_name, sizeof(dir->entry.d_name), NULL, NULL);
+    if (FindNextFileW(dir->find_handle, &dir->find_data) == 0) {
+        if (GetLastError() != ERROR_NO_MORE_FILES) {
+            return NULL;
+        }
+        dir->end = 1;
+    }
+
+    return &dir->entry;
+}
+
+int32_t kx_close_dir(kx_dir_t *dirimpl)
+{
+    if (dirimpl == NULL) {
+        return 0;
+    }
+    kx_dir_impl_t *dir = (kx_dir_impl_t *)dirimpl;
+    if (dir->find_handle != INVALID_HANDLE_VALUE) {
+        FindClose(dir->find_handle);
+    }
+    kx_free(dir);
+    return 1;
+}
+
 #else
 int file_exists(const char *p)
 {
@@ -57,4 +162,26 @@ char* get_exe_path(void)
 
     return s_result;
 }
+
+kx_dir_t* kx_open_dir(const char *path)
+{
+    return opendir(path);
+}
+
+kx_dirent_t* kx_read_dir(kx_dir_t *dir)
+{
+    if (dir == NULL) {
+        return NULL;
+    }
+    return readdir(dir);
+}
+
+int32_t kx_close_dir(kx_dir_t *dir)
+{
+    if (dir == NULL || closedir(dir) == -1) {
+        return 0;
+    }
+    return 1;
+}
+
 #endif
