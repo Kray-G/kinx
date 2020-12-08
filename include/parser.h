f2cd09c89b19f3c1bef85d959b3966ac0fe19cc5
Kray-G <nagai.kazuya@gmail.com>
2020-12-08 16:18:24 +0900

    supported array of double in native.

--- include/parser.h
+++ include/parser.h
@@ -1,4 +1,17 @@
 
+#ifndef KX_PARSER_H
+#define KX_PARSER_H
+
 #include <stdint.h>
 #include <kinx.h>
+
+typedef struct arytype_t_ {
+    int type;
+    int depth;
+} arytype_t;
+
+#ifndef KX_PARSER
 #include <parser.tab.h>
+#endif /* KX_PARSER */
+
+#endif /* KX_PARSER_H */
