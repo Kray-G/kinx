87a28b59a49d1fea630405b4fc8f7a8821813001
Kray-G <nagai.kazuya@gmail.com>
2020-12-08 16:20:00 +0900

    modified a version number.

--- src/mainlib.c
+++ src/mainlib.c
@@ -18,10 +18,10 @@
 #define VER_MAJ 0
 #endif
 #ifndef VER_MIN
-#define VER_MIN 15
+#define VER_MIN 16
 #endif
 #ifndef VER_PAT
-#define VER_PAT 3
+#define VER_PAT 0
 #endif
 #ifndef VER_SUFFIX
 #define VER_SUFFIX ""
