ae69c5229dacae7880bbd4f92537c43a47598b30
Kray-G <nagai.kazuya@gmail.com>
2020-12-11 16:44:48 +0900

    added version control script.

---
+++ include/version.h
@@ -0,0 +1,14 @@
+#ifndef KX_VERSION_H
+
+#if defined(_WIN32) || defined(_WIN64)
+#define PROGNAME "kinx.exe"
+#else
+#define PROGNAME "kinx"
+#endif
+#define VER_MAJ     0
+#define VER_MIN     17
+#define VER_PAT     0
+#define VER_SUFFIX  ""
+
+#endif /* KX_VERSION_H */
+
