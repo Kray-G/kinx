60d6b7a22bd3a425e8c0b4c5b85a0045b881d327
Kray-G <nagai.kazuya@gmail.com>
2020-12-18 12:26:32 +0900

    updated kxcmd.

--- src/main_kxcmd.c
+++ src/main_kxcmd.c
@@ -9,6 +9,28 @@ kx_realloc_t kx_realloc = NULL;
 kx_calloc_t kx_calloc = NULL;
 kx_free_t kx_free = NULL;
 
+char *make_cmd_name(const char *av0)
+{
+    int start = 0, end = 0;
+    const char *p = av0;
+    while (*p) {
+        if (*p == '/' || *p == '\\') {
+            start = p - av0 + 1;
+        } else if (*p == '.') {
+            end = p - av0;
+        }
+        ++p;
+    }
+    if (end <= start) {
+        end = p - av0;
+    }
+    int len = end - start;
+    char *cmdname = kx_calloc(len + 3/* .kx */ + 2, sizeof(char));
+    strncpy(cmdname, av0 + start, len);
+    strcat(cmdname, ".kx");
+    return cmdname;
+}
+
 char *make_exec_name(const char *av0)
 {
     int start = 0, end = 0;
@@ -40,16 +62,25 @@ int main(int ac, char **av)
 
     int r = 0;
     int ac1 = ac + 1;
+    char *cmd = make_cmd_name(av[0]);
     char **avp = kx_calloc(ac1, sizeof(char *));
-    for (int i = 1; i < ac; ++i) {
-        avp[i+1] = av[i];
+    if (file_exists(cmd)) {
+        for (int i = 1; i < ac; ++i) {
+            avp[i+1] = av[i];
+        }
+        avp[0] = av[0];
+        avp[1] = cmd;
+    } else {
+        kx_free(cmd);
+        for (int i = 1; i < ac; ++i) {
+            avp[i+1] = av[i];
+        }
+        avp[0] = av[0];
+        avp[1] = cmd = make_exec_name(av[0]);
     }
-    avp[0] = av[0];
-    avp[1] = make_exec_name(av[0]);
-    if (avp[1]) {
-        r = kinx_call_main(ac1, avp);
-        kx_free(avp[1]);
-    }
+
+    r = kinx_call_main(ac1, avp);
+    kx_free(cmd);
     free(avp);
     return r;
 }
