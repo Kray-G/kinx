06edd90b6644c815a5bdf0af14109c53fbce160a
Kray-G <nagai.kazuya@gmail.com>
2021-01-05 08:44:07 +0900

    improved an error report.

--- include/kxoptimizer.h
+++ include/kxoptimizer.h
@@ -8,6 +8,6 @@ extern void opt_ast_constant_folding(kx_context_t *ctx, kx_object_t *node);
 
 typedef void (*opt_code_func_t)(kvec_pt(kx_code_t) *fixcode, int start);
 extern void opt_code_remove_jmp(kvec_pt(kx_code_t) *fixcode, int start);
-extern void opt_code_otimize_jmp(kvec_pt(kx_code_t) *fixcode, int start);
+extern void opt_code_optimize_jmp(kvec_pt(kx_code_t) *fixcode, int start);
 
 #endif /* KX_KXOPTIMIZER_H */
