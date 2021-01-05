915708ad0c2aecd843cce2a5977c335dd1179562
Kray-G <nagai.kazuya@gmail.com>
2021-01-05 17:13:12 +0900

    added a debugger.

---
+++ src/ir_execdbg.c
@@ -0,0 +1,100 @@
+#include <dbg.h>
+#include <stdio.h>
+#include <math.h>
+#include <inttypes.h>
+#include <kvec.h>
+#include <kstr.h>
+#include <kinx.h>
+#include <kxirutil.h>
+#define KX_DEBUGEXEC_CODE 1
+#include <kxexec.h>
+
+#include "exec/code/_except.inc"
+#include "exec/code/_inlines.inc"
+#include "exec/code/haltnop.inc"
+#include "exec/code/enter.inc"
+#include "exec/code/call.inc"
+#include "exec/code/ret.inc"
+#include "exec/code/throw.inc"
+#include "exec/code/catch.inc"
+#include "exec/code/jmp.inc"
+#include "exec/code/push.inc"
+#include "exec/code/pop.inc"
+#include "exec/code/store.inc"
+#include "exec/code/bnot.inc"
+#include "exec/code/not.inc"
+#include "exec/code/neg.inc"
+#include "exec/code/inc.inc"
+#include "exec/code/dec.inc"
+#include "exec/code/mkary.inc"
+#include "exec/code/append.inc"
+#include "exec/code/apply.inc"
+#include "exec/code/add.inc"
+#include "exec/code/sub.inc"
+#include "exec/code/pow.inc"
+#include "exec/code/mul.inc"
+#include "exec/code/div.inc"
+#include "exec/code/mod.inc"
+#include "exec/code/and.inc"
+#include "exec/code/or.inc"
+#include "exec/code/xor.inc"
+#include "exec/code/shl.inc"
+#include "exec/code/shr.inc"
+#include "exec/code/eqeq.inc"
+#include "exec/code/neq.inc"
+#include "exec/code/le.inc"
+#include "exec/code/lt.inc"
+#include "exec/code/ge.inc"
+#include "exec/code/gt.inc"
+#include "exec/code/lge.inc"
+#include "exec/code/regeq.inc"
+KX_IR_NAME_DEF();
+
+static kx_fnc_t *kx_debug_hook(kx_context_t *ctx, kx_code_t *cur);
+
+#define KEX_DEBUG_HOOK() \
+    if (ctx->location.line != cur->line) { \
+        kx_fnc_t *fn = kx_debug_hook(ctx, cur); \
+        if (fn) { \
+            cur = kx_goto_function(ctx, &caller, cur, fn); \
+            KX_GOTO(); \
+        } \
+        ctx->location.line = cur->line; \
+    } \
+/**/
+
+#define KX_IREXEC_FUNCTION_NAME ir_dbg_exec
+#define KX_IREXEC_IMPL_FUNCTION_NAME ir_dbg_exec_impl
+#include "ir_exec.inl"
+
+static kx_fnc_t *kx_debug_hook(kx_context_t *ctx, kx_code_t *cur)
+{
+    const char *cfile = cur->file;
+    int cline = cur->line;
+    if (ctx->options.debug_step_in_progress) {
+        if (strcmp(ctx->location.file, cfile) != 0) {
+            ctx->location.line = cline;
+            ctx->location.file = cfile;
+            kx_obj_t *obj = allocate_obj(ctx);
+            KEX_SET_PROP_CSTR(obj, "file", cfile);
+            KEX_SET_PROP_INT(obj, "line", cline);
+            push_obj(ctx->stack, obj);
+            ctx->options.debug_step_in_progress = 0;    // make it off once.
+            return ctx->objs.debugger_hook;
+        }
+    }
+    kx_location_list_t *breakpoints = ctx->breakpoints;
+    while (breakpoints) {
+        if (breakpoints->location.line == cline && !strcmp(breakpoints->location.file, cfile)) {
+            ctx->location.line = cline;
+            ctx->location.file = cfile;
+            kx_obj_t *obj = allocate_obj(ctx);
+            KEX_SET_PROP_CSTR(obj, "file", breakpoints->location.file);
+            KEX_SET_PROP_INT(obj, "line", breakpoints->location.line);
+            push_obj(ctx->stack, obj);
+            return ctx->objs.debugger_hook;
+        }
+        breakpoints = breakpoints->next;
+    }
+    return NULL;
+}
