08f529b5d05662fe8666bbaab03e596481788f1a
Kray-G <nagai.kazuya@gmail.com>
2021-01-22 10:58:38 +0900

    updated Makefile for Windows.

--- make.cmd
+++ make.cmd
@@ -2,9 +2,16 @@
 pushd build
 call make.cmd %*
 if ERRORLEVEL 1 goto END
+if "%1"=="" goto COPY
+if "%1"=="all" goto COPY
+if "%1"=="rebuild" goto COPY
+goto END
+
+:COPY
 copy /y kinx.exe ..
 copy /y kxrepl.exe ..
 copy /y kxtest.exe ..
 copy /y *.dll ..
+
 :END
 popd
