59f6029ddf813b92ec0d33f7208b07cffbcba932
Kray-G <nagai.kazuya@gmail.com>
2021-01-22 10:46:43 +0900

    updated a folder change to build.

--- build/make.cmd
+++ build/make.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 if "%1" == "" (
-    if exist "kinx.exe" (
-        kinx.exe utility\VersionSetup.kx 0.19.3
+    if exist "..\kinx.exe" (
+        ..\kinx.exe utility\VersionSetup.kx 0.20.0
     )
     cl.exe /DWINMAIN /Feaddpath.exe utility\src\addpath.c Advapi32.lib User32.lib /link /SUBSYSTEM:WINDOWS
     cl.exe /Feaddpathc.exe utility\src\addpath.c Advapi32.lib User32.lib
