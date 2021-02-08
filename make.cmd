4e9ba2738deca5a228334edb34dec2c08624c51b
Kray-G <nagai.kazuya@gmail.com>
2021-02-08 14:57:12 +0900

    added to generate an installer

--- make.cmd
+++ make.cmd
@@ -1,4 +1,7 @@
 @echo off
+if "%1"=="installer" goto INSTALLER
+if "%1"=="inst" goto INSTALLER
+
 pushd build
 call make.cmd %*
 if ERRORLEVEL 1 goto END
@@ -15,6 +18,12 @@ copy /y kxtest.exe ..
 copy /y addpath.exe ..
 copy /y kx.lib ..
 copy /y *.dll ..
+goto END
+
+:INSTALLER
+if exist "c:\Program Files (x86)\NSIS\Bin\makensis.exe" (
+    "c:\Program Files (x86)\NSIS\Bin\makensis.exe" install.nsi
+)
 
 :END
 popd
