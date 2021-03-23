e6ccf3934ac47ba66b5c3480d8ec0eb8b015988c
Kray-G <nagai.kazuya@gmail.com>
2021-03-24 08:59:19 +0900

    sharing implementation of conversion.

--- src/extlib/kxinteger.c
+++ src/extlib/kxinteger.c
@@ -115,46 +115,6 @@ int Integer_parseInt(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx
     KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object to convert to integer");
 }
 
-int Integer_parseDouble(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
-{
-    if (args > 0) {
-        kvec_t(kx_val_t) *stack = &(ctx->stack);
-        kx_val_t val = kv_last_by(*stack, 1);
-        if (val.type == KX_UND_T) {
-            KX_ADJST_STACK();
-            push_d(ctx->stack, 0.0);
-            return 0;
-        } else if (val.type == KX_INT_T) {
-            double v = (double)val.value.iv;
-            KX_ADJST_STACK();
-            push_d(ctx->stack, v);
-            return 0;
-        } else if (val.type == KX_DBL_T) {
-            KX_ADJST_STACK();
-            push_d(ctx->stack, val.value.dv);
-            return 0;
-        } else if (val.type == KX_BIG_T) {
-            double v = BzToDouble(val.value.bz);
-            KX_ADJST_STACK();
-            push_d(ctx->stack, v);
-            return 0;
-        } else if (val.type == KX_CSTR_T) {
-            double v = strtod(val.value.pv, NULL);
-            KX_ADJST_STACK();
-            push_d(ctx->stack, v);
-            return 0;
-        } else if (val.type == KX_STR_T) {
-            double v = strtod(ks_string(val.value.sv), NULL);
-            KX_ADJST_STACK();
-            push_d(ctx->stack, v);
-            return 0;
-        }
-        KX_THROW_BLTIN_EXCEPTION("ConvertException", "Converting failed to double");
-    }
-
-    KX_THROW_BLTIN_EXCEPTION("SystemException", "Invalid object to convert to double");
-}
-
 int Integer_toString(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
 {
     if (args > 0) {
@@ -192,8 +152,6 @@ int Integer_length(int args, kx_frm_t *frmv, kx_frm_t *lexv, kx_context_t *ctx)
 static kx_bltin_def_t kx_bltin_info[] = {
     { "length", Integer_length },
     { "parseIntImpl", Integer_parseInt },
-    { "parseDouble", Integer_parseDouble },
-    { "toDouble", Integer_parseDouble },
     { "toString", Integer_toString },
 };
 
