9a10e8beac8e93c301d0e71375c3d3703c31b377
Kray-G <nagai.kazuya@gmail.com>
2021-03-31 17:29:37 +0900

    fixed #258: do a folding during analyzing.

--- include/kxoptimizer.h
+++ include/kxoptimizer.h
@@ -4,7 +4,7 @@
 #include <kinx.h>
 
 typedef void (*opt_ast_func_t)(kx_context_t *ctx, kx_object_t *node);
-extern void opt_ast_constant_folding(kx_context_t *ctx, kx_object_t *node);
+extern int opt_ast_constant_folding(kx_context_t *ctx, kx_object_t *node);
 
 typedef void (*opt_code_func_t)(kvec_pt(kx_code_t) *fixcode, int start);
 extern void opt_code_remove_jmp(kvec_pt(kx_code_t) *fixcode, int start);
