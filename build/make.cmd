b65ff3bab5fa7be6731f777d7a35b37b60b6f167
Kray-G <nagai.kazuya@gmail.com>
2021-05-21 09:23:12 +0900

    bumped the version to 1.0.2

--- build/make.cmd
+++ build/make.cmd
@@ -2,7 +2,7 @@
 
 if "%1" == "" (
     if exist "..\kinx.exe" (
-        ..\kinx.exe utility\VersionSetup.kx 1.0.1
+        ..\kinx.exe utility\VersionSetup.kx 1.0.2
     )
     cl.exe /DWINMAIN /Feaddpath.exe utility\src\addpath.c Advapi32.lib User32.lib /link /SUBSYSTEM:WINDOWS
     cl.exe /Feaddpathc.exe utility\src\addpath.c Advapi32.lib User32.lib
